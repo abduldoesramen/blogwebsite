87334fd685af7bd2ed35d6ef40d0488389138f1e
abdul <abdulrehman133782@gmail.com>
2023-01-17 12:38:51 +1100

    added table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Charlie</td>
+          <td>Janitor</td>
+        </tr>
+        <tr>
+          <td>Mac</td>
+          <td>Bouncer</td>
+        </tr>
+        <tr>
+          <td>Dee</td>
+          <td>Aspiring actress</td>
+        </tr>
+        <tr>
+          <td>Dennis</td>
+          <td>Bartender</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default App;
