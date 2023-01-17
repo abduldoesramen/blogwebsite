0c47b1325aa5da0adf36205f1cfb95d3cf181ce1
abdul <abdulrehman133782@gmail.com>
2023-01-17 12:42:41 +1100

    changed Table into its own file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
+import Table from "./Table";
+
 const App = () => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    </table>
-  );
+  return <Table />;
 };
 
 export default App;
