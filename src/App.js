7a514cc3097ca94516cc00e6db9340e6bf617d0b
abdul <abdulrehman133782@gmail.com>
2023-01-20 11:15:17 +1100

    added data from json Array replicating API data grabbing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import Table from "./Table";
 
+const characters = [
+  {
+    name: "Charlie",
+    job: "Job",
+  },
+  {
+    name: "Mac",
+    job: "Bouncer",
+  },
+  {
+    name: "Dee",
+    job: "Aspiring actress",
+  },
+  {
+    name: "Dennis",
+    job: "Bartender",
+  },
+];
+
 const App = () => {
   return (
     <div>
-      <Table />
+      <Table characterData={characters} />
     </div>
   );
 };
--- src/Table.js
+++ src/Table.js
@@ -1,3 +1,5 @@
+import App from "./App.js";
+
 const TableHeader = () => {
   return (
     <thead>
@@ -9,35 +11,29 @@ const TableHeader = () => {
   );
 };
 
-const TableBody = () => {
-  return (
-    <tbody>
-      <tr>
-        <td>Charlie</td>
-        <td>Janitor</td>
+const TableBody = (props) => {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
       </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
-      </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
-    </tbody>
-  );
+    );
+  });
+
+  return <tbody>{rows}</tbody>;
 };
 
-const Table = () => {
+const Table = (props) => {
+  const { characterData } = props;
+
   return (
-    <table>
-      <TableHeader />
-      <TableBody />
-    </table>
+    <div>
+      <table>
+        <TableHeader />
+        <TableBody characterData={characterData} />
+      </table>
+    </div>
   );
 };
 
